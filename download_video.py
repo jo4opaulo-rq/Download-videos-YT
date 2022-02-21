1c9cb6ebd392d6952ecc034e4558582411627cff
João Paulo <joaoroque317@gmail.com>
2022-02-20 22:00:37 -0300

    Update download_video.py

--- download_video.py
+++ download_video.py
@@ -3,7 +3,7 @@ import os
 
 
 def downloadYouTube(videourl, path):
-
+    
     yt = YouTube(videourl)
     yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
     if not os.path.exists(path):
