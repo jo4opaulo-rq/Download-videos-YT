79e0ed3563d39b1374f99b332b7ea4e3fd7ff1f3
João Paulo <joaoroque317@gmail.com>
2022-02-20 21:53:51 -0300

    Update download_video.py

--- download_video.py
+++ download_video.py
@@ -6,8 +6,10 @@ def downloadYouTube(videourl, path):
 
     yt = YouTube(videourl)
     yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    if not os.path.exists(path):
+        os.makedirs(path)
     yt.download(path)
 
 video= input("Link do video: ")
 
-downloadYouTube(video, './Downloads/')
+downloadYouTube(video, './Downloads')
